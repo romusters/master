cd07f30799c93fa81254bc9246b39aaa2b6d1b26
robert peregrine <romusters@gmail.com>
2016-06-15 18:00:57 +0200

    batch script

--- peregrine/batch_script.sh
+++ peregrine/batch_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=04:30:00
+#SBATCH --time=05:30:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=24
-#SBATCH --job-name=w2vVectors
-#SBATCH --mem=2000
+#SBATCH --ntasks=2
+#SBATCH --job-name=w2vDistances
+#SBATCH --mem=80000
 #SBATCH --partition=nodes
 module load Python/3.4.2-goolfc-2.7.11
 module load gensim
