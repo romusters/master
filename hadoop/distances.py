7b7491e864566e2bfc2a8baaaebd7d4ad164b4aa
romusters <romusters@gmail.com>
2016-06-16 22:11:26 +0200

    hadoop cosine similarity of each word between two tweets.

--- hadoop/distances.py
+++ hadoop/distances.py
@@ -18,7 +18,6 @@ conf = (SparkConf()
 
 sc = SparkContext(appName='distances', conf=conf)
 
-
 import filter
 from pyspark.mllib.feature import Word2Vec, Word2VecModel
 # load data
@@ -36,11 +35,47 @@ sqlContext = SQLContext(sc)
 lookup = sqlContext.read.parquet('/user/rmusters/2015model99/data').alias("lookup")
 lookup_bd = sc.broadcast(lookup.rdd.collectAsMap())
 
+#get the vectors for the tweets, which are not averaged (or some other manner) yet
+a = "man"
+b = "vrouw"
 
 vectors = data.map(lambda ws: [lookup_bd.value.get(w) for w in ws])
 logger.info(vectors.count())
-path =  'hdfs:///user/rmusters/distances.txt'
-vectors.saveAsTextFile(path)
+
+#save distances
+path =  'hdfs:///user/rmusters/distances'
+from pyspark.sql import Row
+row = Row("vectors") # Or some other column name
+rows = vectors.map(row).toDF()
+rows.write.parquet(path, mode="overwrite")
+
+
+
+
+text = text_file.take(10)
+data = sc.parallelize(text).map(lambda line: (line, filter.filter(line).split(" "))).map(lambda (text, filtered): (text, filtered, [lookup_bd.value.get(w) for w in filtered][0]))
+from pyspark.sql.functions import monotonicallyIncreasingId
+df = data.toDF(["text", "filtered_text", "vectors"])
+# This will return a new DF with all the columns + id
+res = df.withColumn("id", monotonicallyIncreasingId())
+vectors = df.select("vectors").collect()
+
+
+def similarity(tweet, other_tweet):
+	import numpy
+	sims = []
+	for word in tweet:
+		tmp_cos_sim = 0
+		for other_word in other_tweet:
+			cos_sim = numpy.dot(model.transform(word), model.transform(other_word)) / (numpy.linalg.norm(model.transform(word)) * numpy.linalg.norm(model.transform(other_word)))
+			if cos_sim > tmp_cos_sim:
+				tmp_cos_sim = cos_sim
+		sims.append(tmp_cos_sim)
+	return sum(sims)/len(sims)
+
+# assume that tweets with the same topic, have words with the same semantic meaning at the same location
+# take a tweet and calculate the distance to all the other tweets. Be sure to take the tweet with the most words and compare each word.
+
 # res = vectors.take(2)
 #
 # vectors = data.map(lambda line: model.transform(line[0]))
