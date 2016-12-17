hopkins normalisation fix. w2v stopwords fix. Active Learning.

al.py
@@ -1,106 +1,81 @@
 import logging
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
 def active_learning(seed_id, threshold):
 	logging.info("Start Active Learning")
 	import pandas as pd
-
+	store = pd.HDFStore("/media/cluster/data1/lambert/data_sample_vector_id.clean.h5")
+	tweets = pd.read_csv("/media/cluster/data1/lambert/lambert_w2v_data_jan_tweet_id.csv", header=None)
+	tokens = pd.read_csv("/media/cluster/data1/lambert/data_sample_tokens_id.csv", header=None)
 	#get the tweet with similarities
 	sim_name = "/media/cluster/data1/lambert/lambert_w2v_data_jan_sims_" + str(seed_id) + ".csv"
 	tweet_sims = pd.read_csv(sim_name, header=None)
 
-	# get all the indices, the embeddings will be checked on uniqueness
+	# get all the indices of the tweets which are more similar than the threshold
 	yes_idx = tweet_sims[tweet_sims[1] > threshold][0].values.tolist()
-	print yes_idx
 	logging.info("Amount of positive tweets found: %i", len(yes_idx))
 
-	# get the embedding belonging to ids
-	store = pd.HDFStore("/media/cluster/data1/lambert/lambert_w2v_data_jan_all_columns.csv.clean.h5")
+	# get the embedding belonging to the indices
 	yes_data = store.select("data", where=store["data"]["id"].isin(yes_idx))
 	logging.info("Amount of tweets found in the store: %i", yes_data.shape[0])
+
+	# make sure that the similarity file did not get appended by accident
+	assert(len(yes_idx) == yes_data.shape[0])
 	dim = yes_data.shape[1]-1
-	# get the tweet ids with the highest similarities and ...
-	#note that the row indices in the df below are not tweet ids.
 
-	if len(yes_idx) > 20:
-		logging.info( "sample of 20")
-		yes_sample = yes_data.sample(20)
-		yes_idx = yes_sample["id"]
-		yes_embs = yes_sample[range(dim)]
-	else:
-		logging.info("sample smaller than 20")
-		yes_idx = yes_data["id"]
-		yes_embs = yes_data[range(dim)]
+	#get the sample of the tweets which are similar
+	yes_idx, yes_embs = get_sample(yes_idx, yes_data, dim)
 
-	tweets = pd.read_csv("/media/cluster/data1/lambert/lambert_w2v_data_jan_tweet_id.csv", header=None)
 	yes_tweets = tweets[tweets[0].isin(yes_idx)].values.tolist()
-	for tweet in yes_tweets:
-		logging.info(tweet)
-	yes_tokens = pd.read_csv("/media/cluster/data1/lambert/data_sample_tokens_id.csv", header=None)
-	yes_tokens = yes_tokens[yes_tokens[0].isin(yes_idx)].values.tolist()
-	vocab = w2v.get_vocab()
-	for token in yes_tokens:
-		token = token[1].split()
-		res = []
-		for t in token:
-			if t in vocab:
-				res.append(t)
-		logging.info(res)
+	# log_tweets(yes_tweets)
 
+	import single_char
+	getch = single_char._Getch()
+	print "Press y (yes) or n (no)"
+	input_labels = []
+	for tweet in yes_tweets:
+		print tweet
+		val = getch.__call__()
+		if val == "y":
+			val = [0, 1]
+		elif val == "n":
+			val = [1, 0]
+
+		input_labels.append( val )
+	logging.info("input labels %s" , str(input_labels))
+
+
+	yes_tokens = tokens[tokens[0].isin(yes_idx)].values.tolist()
+	vocab = w2v.get_vocab()
+	# log_tokens(yes_tokens, vocab)
 
 	# check if dimension of model and data are the same
-	model_name = "/media/cluster/data1/lambert/lambert_model.csv.h5"
-	model_store = pd.HDFStore(model_name)
-	dim_model = len(model_store["data"]["vectors"][0])
-	dim_data = yes_embs.shape[1]
-
-	logging.info(dim_model)
-	logging.info(dim_data)
-
-	assert(dim_model == dim_data)
-	dim = dim_data
+	check_dims(yes_embs)
 
 	yes_embs = yes_embs.values.tolist()
 	logging.info("Number of datapoints is %i and the dimension is %i", len(yes_embs), len(yes_embs[0]))
+
+	# create variables to hold the Tensorflow data
 	data = yes_embs
 	labels = []
 	labels.extend([[0,1]]*len(yes_embs))
-	# yes.append(pd.DataFrame(data=[dim * [[0, 1]]]).T)
 
-	no_idx = tweet_sims[tweet_sims[1] < 0.5][0].values.tolist()
-	logging.info(no_idx[0])
+	no_idx = tweet_sims[(tweet_sims[1] < 0.0) & (tweet_sims[1] > -1.0)][0].values.tolist()
 	no_data = store.select("data", where=store["data"]["id"].isin(no_idx))
 
-	if no_data.shape[0]>20:
-		logging.info("sample of 20")
-		no_sample = no_data.sample(20)
-		# no_idx = no_sample["id"]
-		logging.debug(no_idx)
-		no_embs = no_sample[range(dim)]
-	else:
-		logging.info( "sample smaller than 20")
-		# no_idx = no_data["id"]
-		no_embs = no_data[range(dim)]
 
+	no_idx, no_embs = get_sample(no_idx, no_data, dim)
 	no_tweets = tweets[tweets[0].isin(no_idx)].values.tolist()
-	# tokens = pd.read_csv("/media/cluster/data1/data_sample_tokens_id.csv", header=None)
-
+	log_tweets(no_tweets)
+	no_tokens = tokens[tokens[0].isin(no_idx)].values.tolist()
+	# log_tokens(no_tokens, vocab)
 	no_embs = no_embs.values.tolist()
-	# for emb in no_embs:
-	# 	logging.debug(no_embs)
-	logging.debug(no_embs[0])
-	logging.debug(no_embs[-1])
 	labels.extend([[1, 0]] * len(no_embs))
 	data.extend(no_embs)
+
 	import numpy as np
 	labels = np.array(labels)
 	data = np.array(data)
-
-	for n in data:
-		logging.debug(n)
-	logging.info("Length of data is %i and the dimension of first element is %i and the last element is %i", len(data), len(data[0]), len(data[-1]))
-
-	# no.append(pd.DataFrame(data=[dim * [[1, 0]]]).T)
-	# batch = yes.append(no)
 
 	import tensorflow as tf
 	sess = tf.InteractiveSession()
@@ -131,23 +106,75 @@ def get_seed(word):
 	logging.info("Seed word is: %s", word)
 	with open("/media/cluster/data1/lambert/lambert_w2v_data_jan_tweet_id.csv", 'rb') as f:
 		for line in f:
-			if word in line:
+			# if word in line:
+			import re
+			if re.search(r"\b%s\b" % word, line):
 				print line
 				idx = int(line.split(",")[0])
 				break
 	logging.info("Seed id is %i", idx)
 	return idx
 
-import w2v
-word = "moslim"
-# word = "vegan"
-# word = "rutte"
-# word = "nieuwjaar"
-# word = "1"
-# word = "overheid"
-seed_id = get_seed(word)
-embs_name = "/media/cluster/data1/lambert/lambert_w2v_data_jan_all_columns.csv.clean.h5"
-w2v.predict(embs_name, seed_id)
+def log_tweets(tweets):
+	for tweet in tweets:
+		logging.info(tweet)
 
-threshold = 0.993
-active_learning(seed_id, threshold)
+
+def log_tokens(tokens, vocab):
+	for token in tokens:
+		token = token[1].split()
+		res = []
+		for t in token:
+			if t in vocab:
+				res.append(t)
+		logging.info(res)
+
+def get_sample(idx, data, dim):
+	if len(idx) > 20:
+		logging.info( "sample of 20")
+		sample = data.sample(20)
+		idx = sample["id"]
+		embs = sample[range(dim)]
+	else:
+		logging.info("sample smaller than 20")
+		idx = data["id"]
+		embs = data[range(dim)]
+	return idx, embs
+
+def check_dims(embs):
+	import pandas as pd
+	model_name = "/media/cluster/data1/lambert/lambert_model.csv.h5"
+	model_store = pd.HDFStore(model_name)
+	dim_model = len(model_store["data"]["vectors"][0])
+	dim_data = embs.shape[1]
+
+	logging.info(dim_model)
+	logging.info(dim_data)
+
+	assert (dim_model == dim_data)
+	dim = dim_data
+
+
+import w2v
+words = ["voetbal", "racisme", "moslim", "veganisme", "politiek", "seks", "duurzame energie"]
+words = ["moslim"]
+for word in words:
+
+	# get a topic using a word
+	seed_id = get_seed(word)
+	import plot
+
+
+
+	# create the similarity file corresponding to the topic
+	embs_name = "/media/cluster/data1/lambert/data_sample_vector_id.clean.h5"
+	# w2v.predict(embs_name, seed_id)
+	# raw_input("Press enter to continue...")
+	# print "Continuing"
+
+	# plot.plot_sims(seed_id, word)
+
+	# find tweets corresponding to the topic and build a classifier
+	threshold = 0.8
+	# active_learning(seed_id, threshold)
+	# break

csv_utils.py
@@ -1,34 +1,3 @@
-def no_use():
-	import pandas as pd
-	d_name = "/media/cluster/data1/model3971/w2v_data_jan_large.csv"
-	id_name = "/media/cluster/data1/model3971/w2v_data_jan.csv.clean.sims.h5"
-	store = pd.HDFStore(id_name)
-	ids = store["data"]["id"].values.tolist()
-
-	data = pd.read_csv(d_name, header=None, iterator=True, chunksize=1000)
-	chunk = data.get_chunk()
-
-	# iterate over the ids we need
-	for id in ids:
-		while chunk is not None:
-			df = chunk[chunk[3].isin([id])]
-			if not df.empty:
-				print df
-
-			else:
-				continue
-			# found_ids = []
-			# found_tweets = []
-			# for row in chunk.iterrows():
-			# 	found_id = row[1][3]
-			# 	if id in ids:
-			# 		found_ids.append(id)
-			# 		print row[1][0]
-			# 		found_tweets.append(row[1][0])
-			chunk = data.get_chunk()
-
-
-
 def data_sample_tweet_id(vector_name, tweet_name):
 	import pandas as pd
 	tweets = pd.read_csv(vector_name, header=None, iterator=True, chunksize=1000, usecols=[0, 1])
@@ -37,8 +6,6 @@ def data_sample_tweet_id(vector_name, tweet_name):
 		chunk.index = chunk[0]
 		chunk[1].to_csv(tweet_name, mode="a")
 		chunk = tweets.get_chunk()
-
-
 
 # when collecting the lookup table from hadoop through the terminal, we get a dataframe which we convert to dict: dict(df)
 def dict_to_csv(dict, fname):
@@ -53,7 +20,6 @@ def convert_lookup_to_hdf(fname):
 	data = pd.read_csv(fname, names=["words", "vectors"])
 	data["vectors"] = data["vectors"].apply(lambda x: eval(x))
 	data.to_hdf(fname + ".h5", "data")
-
 
 
 def data_sample_vector_id(data_name, vector_name):
@@ -78,8 +44,6 @@ def data_sample_vector_id(data_name, vector_name):
 
 def data_sample_tokens_id(data_name):
 	import pandas as pd
-
-
 	tweets = pd.read_csv(data_name, header=None, iterator=True, index_col=False, chunksize=1000, usecols=[0,2],
 						 names=["id", "token"])
 	chunk = tweets.get_chunk()
@@ -89,13 +53,98 @@ def data_sample_tokens_id(data_name):
 		chunk = tweets.get_chunk()
 
 
+def rm_wrappedarray():
+	import pandas as pd
+	data_name = "/media/cluster/data1/lambert/voetbal_moslim_vectors.csv"
+	data = pd.read_csv(data_name, header=None)
+	data["vectors"]  = data[0].apply(lambda x: eval(x.replace("WrappedArray(", "[").replace(")", "]")))
+	return data
+
+
+def find_subject_tweets(word):
+	import pandas as pd
+	import re
+	data_name = "/media/cluster/data1/lambert/data_sample_tokens_id.csv"
+	data = pd.read_csv(data_name, header=None, names=[ "id", "tokens"])
+
+	data =data[data["tokens"].str.contains(word)]
+	ids = data["id"].values.tolist()
+	print len(ids)
+	cluster_name = "/media/cluster/data1/lambert/cluster_id.csv"
+	cluster = pd.read_csv(cluster_name, header=None, names=["cluster", "id"])
+	# which cluster has the max number of word occurrences
+	c_ids = pd.merge(data, cluster, on="id")
+	print c_ids["cluster"].value_counts().index.tolist()[0:2]
+	return c_ids["cluster"].value_counts().index.tolist()[0]
+
+def show_cluster_tweets(cluster_id):
+	import pandas as pd
+	cluster_name = "/media/cluster/data1/lambert/cluster_id.csv"
+	cluster = pd.read_csv(cluster_name, header=None, names=["cluster", "id"])
+	tweet_ids = cluster[cluster["cluster"] == cluster_id].drop_duplicates()
+	print tweet_ids
+	tweet_name = "/media/cluster/data1/lambert/lambert_w2v_data_jan_tweet_id.csv"
+	# tweet_name = "/media/cluster/data1/lambert/data_sample_tokens_id.csv"
+
+	tweet_data = pd.read_csv(tweet_name, header=None, names=["id", "text"])
+	print tweet_data
+	res = pd.merge(tweet_data, tweet_ids)
+	for tweet in res["text"]:
+		if "voetbal" not in tweet:
+			print tweet
+
+def show_cluster_homogenity():
+	import pandas as pd
+	cluster_name = "/media/cluster/data1/lambert/cluster_id.csv"
+	cluster_data = pd.read_csv(cluster_name, header=None, names=["cluster", "id"])
+	vector_name = "/media/cluster/data1/lambert/data_sample_vector_id"
+	store = pd.HDFStore(vector_name + ".clean.h5")
+	vector_data = store["data"]
+	hopkins_list = []
+	for i in range(500):
+		print i
+		# get the ids for each cluster
+		ids = cluster_data[cluster_data.cluster == i]
+		# get the vectors for each cluster
+		data = pd.merge(ids, vector_data, on="id")[range(70)]
+		# Calculate Hopkins for each cluster
+		import hopkins
+		import numpy as np
+		hop = hopkins.hopkins(np.array(data.values.tolist()), i)
+		print hop["hopkins"]
+		hopkins_list.append(hop["hopkins"])
+		print
+	print hopkins_list
+
+
+def input_cluster_topics():
+	f = open("/media/cluster/data1/lambert/hopkins_result")
+	g = open("/media/cluster/data1/lambert/topics", "a")
+	g.writelines("cluster, topic\n")
+	g.close()
+	hopkins = eval(f.read())
+	hopkins.reverse()
+	hopkins =hopkins[32:500]
+	for h in hopkins:
+		show_cluster_tweets(h)
+		x = raw_input()
+		g = open("/media/cluster/data1/lambert/topics", "a")
+		g.write(str(h) + "," + x + "\n")
+		g.close()
+
 if __name__ == "__main__":
 	data_name = "/media/cluster/data1/lambert/lambert_w2v_data_jan_all_columns.csv"
-	data_name = "/media/cluster/data1/lambert/lambert_w2v_data_jan_all_columns3.csv"
 	vector_name = "/media/cluster/data1/lambert/data_sample_vector_id"
-	data_sample_vector_id(data_name, vector_name)
+	# data_sample_vector_id(data_name, vector_name)
 	tweet_name = "/media/cluster/data1/lambert/lambert_w2v_data_jan_tweet_id.csv"
 	# data_sample_tweet_id(data_name, tweet_name)
 	# data_sample_tokens_id(data_name)
 	model_name = "/media/cluster/data1/lambert/lambert_model.csv"
 	# convert_lookup_to_hdf(model_name)
+
+	word = "vegan"
+	# cluster_id = find_subject_tweets(word)
+	# show_cluster_tweets(cluster_id)
+
+	# show_cluster_homogenity()
+	input_cluster_topics()

hadoop/w2v.py
@@ -7,6 +7,8 @@ import logging, sys
 # import filter
 
 #spark-submit --py-files master/hadoop/stemmer.py,master/hadoop/filter.py --packages com.databricks:spark-csv_2.10:1.4.0 --master yarn --executor-memory 20g --deploy-mode cluster  master/hadoop/w2v.py
+#spark-submit --master yarn --deploy-mode cluster --packages com.databricks:spark-csv_2.10:1.4.0 --driver-memory 50g --executor-memory 10g --num-executors 100 w2v.py
+
 #if in yarn mode, all cores are used
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
@@ -17,13 +19,14 @@ loc = '/user/rmusters/text/2015/01/*'
 conf = (SparkConf()
     .set("spark.driver.maxResultSize", "0")\
 	.set("spark.driver.memory", "50g")\
-	.set("spark.executor.instances", "10")\
+	.set("spark.executor.instances", "200")\
 	.set("spark.executor.cores", "2")\
-	.set("spark.rpc.askTimeout", "120000") \
-	.set("spark.akka.frameSize", "300")\
-	.set("spark.executor.heartbeatInterval", "1200000"))
+	.set("spark.rpc.askTimeout", "600000") \
+	.set("spark.akka.frameSize", "600")\
+	.set("spark.executor.heartbeatInterval", "600000") \
+	.set("spark.executor.heartbeat", "600000"))
 
-sc = SparkContext(appName='Word2Vec', conf=conf)
+sc = SparkContext(appName='Word2Vec')
 sqlContext = SQLContext(sc)
 
 def main():
@@ -90,9 +93,6 @@ def average_word_vectors(vectors):
 
 
 def mean_slice(slice):
-	try:
-	except RuntimeWarning:
-		return None
 	import numpy as np
 	if len(slice) == 0:
 		print "zero slice found"
@@ -100,23 +100,21 @@ def mean_slice(slice):
 	else:
 		return np.mean(slice, axis = 0).tolist()
 
-def average_word_vectors():
-	import numpy as np
-	# lookup_bd = load_model('/user/rmusters/jan_threshold20_2015model99')
-	lookup_bd = load_model('/user/rmusters/lambert_jan_2015model')
-	data = sqlContext.read.parquet("/user/rmusters/data_jan_sample")
-	data = data.map(lambda (text, filtered_text, id):  (text, [w for w in filtered_text.split() if not w == None and w != "<STOPWORD>" and w != "<MENTION>" and w !="<URL>"and w in lookup_bd.value.keys()] , id))
-	data = data.map(lambda (text, filtered_text, id):  (text, filtered_text, np.mean([lookup_bd.value.get(w) for w in filtered_text ], axis=0).tolist(), id))
-# data = data.map(lambda (text, filtered_text, id):  (text, filtered_text, mean_slice([lookup_bd.value.get(w) for w in filtered_text if w in lookup_bd.value.keys()]), id))
 
-	df = data.toDF(["text", "filtered_text", "vectors", "id"])
-	# df.write.parquet("/user/rmusters/w2v_data_jan", mode="overwrite")
-	# df.write.parquet("/user/rmusters/lambert_w2v_data_jan", mode="overwrite")
-	df.save("lambert_w2v_data_jan3.csv", "com.databricks.spark.csv", "overwrite")
+
+def average_word_vectors():
+	lookup_bd = load_model('/user/rmusters/lambert_jan_2015model')
+	keys = lookup_bd.value.keys()
+	data = sqlContext.read.parquet("/user/rmusters/data_jan_sample")
+	data = data.map(lambda (text, filtered_text, id):  (text, filtered_text, [w for w in filtered_text.split() if not w == None and w != "<STOPWORD>" and w != "<MENTION>" and w !="<URL>"and w in keys] , id))
+	data = data.map(lambda (text, filtered_text, split_text, id):  (text, filtered_text, split_text, mean_slice([lookup_bd.value.get(w) for w in split_text ]), id))
+	df = data.toDF(["text", "filtered_text", "split_text", "vectors", "id"])
+	df.write.parquet("hdfs:///user/rmusters/lambert_w2v_data_jan", mode="overwrite")
+	df.select("text", "filtered_text", "vectors", "id").save("lambert_w2v_data_jan.csv", "com.databricks.spark.csv", "overwrite")
+
 
 
 def save_vectors(path):
-	#pyspark --packages com.databricks:spark-csv_2.10:1.4.0
 	vectors = sqlContext.read.parquet(path + '/data')
 	vectors.save("w2v_vectors.csv", "com.databricks.spark.csv")
 
@@ -183,8 +181,10 @@ if __name__ == "__main__":
 	#save_vectors(path)
 	#save_w2v_data()
 	average_word_vectors()
+
 	#train_w2v()
 	# lambert()
+
 
 #from pyspark.mllib.feature import Word2VecModel
 #model = Word2VecModel.load(sc,"word_vec_from_cleaned_query.model")

hopkins.py
@@ -4,9 +4,10 @@ def hopkins(X, id):
 	d = len(X[0]) # kolommen
 	n = len(X) # rijen
 	m = int(0.1 * n)
-
+	import sklearn.preprocessing as pre
+	X = pre.normalize(X)
 	from sklearn.neighbors import NearestNeighbors
-	nbrs = NearestNeighbors(n_neighbors=1, algorithm='brute').fit(X)
+	nbrs = NearestNeighbors(n_neighbors=1, algorithm='brute', metric="cosine").fit(X)
 
 	from random import sample
 	rand_X = sample(range(0, n, 1), m)
@@ -14,11 +15,11 @@ def hopkins(X, id):
 	ujd = []
 	wjd = []
 	for j in range(0, m):
-		u_dist, _ = nbrs.kneighbors(np.random.normal(size=(1, d)).reshape(1, -1), 2, return_distance=True)
+		u_dist, _ = nbrs.kneighbors(pre.normalize(np.random.normal(size=(1, d))).reshape(1, -1), 2, return_distance=True)
 		ujd.append(u_dist[0][1])
 		w_dist, _ = nbrs.kneighbors(X[rand_X[j]].reshape(1, -1), 2, return_distance=True)
 		wjd.append(w_dist[0][1])
-
+	# print ujd, wjd
 	H = sum(ujd)/(sum(ujd)+sum(wjd))
 	hs.append(H)
 
@@ -81,13 +82,33 @@ def get_data(id):
 	X = preprocessing.scale(X)
 	return X, id
 
-clusters = [8, 16, 111, 140, 189, 190, 231]
-#clusters = [235, 253, 263, 340, 353, 362, 366, 412, 433, 441, 458, 497]
-for cluster in clusters:
-	print "id ", cluster
-	print "get data"
-	data, id = get_data(cluster)
-	print "get result"
-	result = hopkins(data, id)
-	print "plot result"
-	plot_hopkins(result, id)
+# clusters = [8, 16, 111, 140, 189, 190, 231]
+# #clusters = [235, 253, 263, 340, 353, 362, 366, 412, 433, 441, 458, 497]
+# for cluster in clusters:
+# 	print "id ", cluster
+# 	print "get data"
+# 	data, id = get_data(cluster)
+# 	print "get result"
+# 	result = hopkins(data, id)
+# 	print "plot result"
+# 	plot_hopkins(result, id)
+
+
+def voetbal_moslim():
+	import csv_utils
+	import numpy as np
+	data = csv_utils.rm_wrappedarray()
+	print hopkins(np.array(data["vectors"].values.tolist()), 0)
+
+# voetbal_moslim()
+
+# import pandas as pd
+# import numpy as np
+# data = pd.HDFStore("/media/cluster/data1/lambert/data_sample_vector_id.clean.h5")["data"]
+# print data.shape
+# sample_size = 5
+# vectors = map(float, np.array(data.sample(sample_size )[range(70)].values.tolist()).ravel())
+# print len(vectors)
+# vectors = np.reshape(vectors, (sample_size , data.shape[1]-1))
+# print vectors
+# hopkins(vectors, 0)

optics.py
@@ -15,7 +15,7 @@ def read_data(id):
 	return np.array(data.tolist())
 
 
-def optics_alg(x, k, distMethod='euclidean'):
+def optics_alg(x, k, distMethod='cosine'): #was euclidean
 	import time
 	tic = time.clock()
 	import numpy as N
@@ -28,7 +28,9 @@ def optics_alg(x, k, distMethod='euclidean'):
 		n == 1
 
 	try:
-		D = H.squareform(H.pdist(x, distMethod))
+		# D = H.squareform(H.pdist(x, distMethod))
+		from scipy.spatial.distance import pdist
+		D = H.squareform(pdist(x, distMethod))
 		distOK = True
 	except:
 		print "squareform or pdist error"
@@ -106,4 +108,11 @@ def main():
 		# ordered_reachabilities = RD[order]
 		# plot_optics(ordered_reachabilities, cluster)
 
-main()
+import pandas as pd
+import numpy as np
+data = pd.HDFStore("/media/cluster/data1/lambert/data_sample_vector_id.clean.h5")["data"]
+vectors = np.array(data.sample(20000)[range(70)].values.tolist())
+RD, CD, order = optics_alg(vectors, 10)
+ordered_reachabilities = RD[order]
+ordered_reachabilities = [ o for o in ordered_reachabilities if o <= 1]
+plot_optics(ordered_reachabilities, 11)

skewness.py
@@ -1,22 +1,49 @@
-def get_first_attr():
-	import numpy as np
-	import sys
-	import csv
-	path = '/home/cluster/data/lda_data_cluster.csv'
+def get_skewness():
+	import pandas as pd
+	dim = 70
+	fname = None
+	data = pd.HDFStore("/media/cluster/data1/lambert/data_sample_vector_id.clean.h5")["data"][range(70)]
+	skewness = data.skew().values.tolist()
+	print skewness
+	return skewness
+
+def plot_skewness(data):
+	from plotly.offline import init_notebook_mode, plot
+	init_notebook_mode()
+	from plotly.graph_objs import Scatter, Figure, Layout
+	dim = len(data)
+	data = get_skewness()
+	trace = Scatter(x=data, y=range(dim), mode="markers", marker=dict(color="rgb(0,0,0)"))
+
+	data = [trace]
+	layout = Layout(title="Insight into skewness", xaxis=dict(title="Feature"),
+					yaxis=dict(title="Skewness"))
+	fig = Figure(data=data, layout=layout)
+	plot(fig)
+
+skewness = get_skewness()
+plot_skewness(skewness)
 
 
-	idx = 0
-	dict = {}
-	import scipy.stats
-	total_skewness = []
-	for i in range(0, 10774, 1):
-		f = open(path, "r")
-		data = csv.reader(f)
-		vals = []
-		print i
-		for row in data:
-			vals.append(eval(row[2])[i])
-		skew = scipy.stats.skew(vals)
-		print skew
-		total_skewness.append(skew)
-	print total_skewness
+# def get_first_attr():
+# 	import numpy as np
+# 	import sys
+# 	import csv
+# 	path = '/home/cluster/data/lda_data_cluster.csv'
+#
+#
+# 	idx = 0
+# 	dict = {}
+# 	import scipy.stats
+# 	total_skewness = []
+# 	for i in range(0, 10774, 1):
+# 		f = open(path, "r")
+# 		data = csv.reader(f)
+# 		vals = []
+# 		print i
+# 		for row in data:
+# 			vals.append(eval(row[2])[i])
+# 		skew = scipy.stats.skew(vals)
+# 		print skew
+# 		total_skewness.append(skew)
+# 	print total_skewness